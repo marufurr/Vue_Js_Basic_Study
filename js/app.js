fa9bcac846694e7de049eff0b9546c24cf02f5d3
Md.Marufur Rahman <marufurr75@gmail.com>
2019-09-19 19:00:18 +0600

    this is basic class

---
+++ js/app.js
@@ -0,0 +1,15 @@
+let app = new Vue({
+  el:"#app",
+  data:{
+    
+      name: "Md.Marufur Rahman",
+      age: 24
+  },
+  methods:{
+    myAge(){
+      return 'learnHunter'
+    }
+
+  }
+  
+});
